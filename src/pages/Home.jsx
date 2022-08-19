74d6aa8882457d55904f76e63e8bd05f7ff5ff0f
Chyanne Robbins <chyannetech@gmail.com>
2022-08-19 00:28:06 -0400

    Complete Home Page CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import "../style/home.css";
 const Home = () =>  {
 return (
 <div className='homePage'>
+<div className="titleHome">travel | moire</div>
     <div className='searchContainer'>
     <input className='searchBox' placeholder='Atlanta, GA'></input>
     <div className='searchIcon'></div>
@@ -20,6 +21,92 @@ return (
     <div className='cafe'>Cafes</div>
     </div>
 
+{/* User Post */}
+    <div className='userPost'>
+    <div className='userBox'>
+    <div className='userPic'></div>
+    <div className='name'>Daisy</div>
+    </div>
+
+    <div className='postBox'>
+    <div className='city'>Atlanta</div>
+    <div className='tb'>Chika, Steve, Tom</div>
+    </div>
+    
+    <div className='postBox2'>
+    <div className='view'>View Media</div>
+    <div className='location'>Piedmont Park</div><div className='reviews'>63.6k posts</div>
+    </div>
+
+    <div className='viewPost'>View Post</div>
+    </div>
+{/* End of User Post */}
+
+
+{/* User Post */}
+<div className='userPost'>
+    <div className='userBox'>
+    <div className='userPic'></div>
+    <div className='name'>Chika</div>
+    </div>
+
+    <div className='postBox'>
+    <div className='city'>Atlanta</div>
+    <div className='tb'>Chika, Steve, Tom</div>
+    </div>
+    
+    <div className='postBox2'>
+    <div className='view'>View Media</div>
+    <div className='location'>Piedmont Park</div><div className='reviews'>63.6k posts</div>
+    </div>
+
+    <div className='viewPost'>View Post</div>
+    </div>
+{/* End of User Post */}
+
+{/* User Post */}
+<div className='userPost'>
+    <div className='userBox'>
+    <div className='userPic'></div>
+    <div className='name'>Kenny</div>
+    </div>
+
+    <div className='postBox'>
+    <div className='city'>Atlanta</div>
+    <div className='tb'>Chika, Steve, Tom</div>
+    </div>
+    
+    <div className='postBox2'>
+    <div className='view'>View Media</div>
+    <div className='location'>Piedmont Park</div><div className='reviews'>63.6k posts</div>
+    </div>
+
+    <div className='viewPost'>View Post</div>
+    </div>
+{/* End of User Post */}
+
+{/* User Post */}
+<div className='userPost'>
+    <div className='userBox'>
+    <div className='userPic'></div>
+    <div className='name'>Steve</div>
+    </div>
+
+    <div className='postBox'>
+    <div className='city'>Atlanta</div>
+    <div className='tb'>Chika, Steve, Tom</div>
+    </div>
+    
+    <div className='postBox2'>
+    <div className='view'>View Media</div>
+    <div className='location'>Piedmont Park</div><div className='reviews'>63.6k posts</div>
+    </div>
+
+    <div className='viewPost'>View Post</div>
+    </div>
+{/* End of User Post */}
+
+<div className='createNew'>Create New Post</div>
 </div>
 )}
 
