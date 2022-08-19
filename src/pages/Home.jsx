fe4b7dd790fd4df0dfa7b61bd275c48a0bcd0d2a
Chyanne Robbins <chyannetech@gmail.com>
2022-08-19 04:54:35 -0400

    Update Mobile CSS Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useState } from "react"
 import { useNavigate } from 'react-router';
 
-const Home = () =>  {
+function Home (){
     
     const [show, setShow] = useState(false);
 
@@ -37,6 +37,7 @@ return (
     <div className='name'>Daisy</div>
     </div>
 
+   
     <div className='postBox'>
     <div className='city'>Atlanta, GA</div>
     <div className='tb'>Chika, Steve, Tom</div>
@@ -46,14 +47,19 @@ return (
     <div className='view'>View Media</div>
     <div className='location'>Piedmont Park</div><div className='reviews'>63.6k posts</div>
     </div>
-    <>
-     
-     <div className='viewPost' onClick={handleShow}>View Post</div>
-
-  
-   </> 
-    </div>
    
+     <div className='viewPost' onClick={handleShow}>View Post</div>
+     
+     <>
+    <Offcanvas show={show} onHide={handleClose} placement={'start'} className="offcanvas">
+       <Offcanvas.Body>
+         Some text as placeholder. In real life you can have the elements you
+         have chosen. Like, text, images, lists, etc.
+       </Offcanvas.Body>
+     </Offcanvas>
+    </>
+   
+    </div>
 {/* End of User Post */}
 
 
@@ -117,19 +123,10 @@ return (
     </div>
 
 
-    <div className='viewPost' onClick={handleShow}>View Post</div>
+    <div className='viewPost' onClick={handleShow}>View Post </div>
     </div>
 {/* End of User Post */}
 
-
-<Offcanvas show={show} onHide={handleClose}>
-       <Offcanvas.Header closeButton>
-       </Offcanvas.Header>
-       <Offcanvas.Body>
-         Some text as placeholder. In real life you can have the elements you
-         have chosen. Like, text, images, lists, etc.
-       </Offcanvas.Body>
-     </Offcanvas>
 
 <div className='createNew'>Create New Post</div>
 </div>
