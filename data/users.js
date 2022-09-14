914f8f40fdf3fca1f98748a0d31dd9654acb61a4
Chyanne Robbins <chyannetech@gmail.com>
2022-09-13 22:28:33 -0400

    Clean-up File Structure

---
+++ data/users.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+        name: "Diasy",
+        city: "Atlanta, GA",
+        media: "View Media",
+        location: "Piedmon Park"
+    },
+    {
+        name: "Chika",
+        city: "Atlanta, GA",
+        media: "View Media",
+        location: "Grant Park"
+    },
+    {
+        name: "Kenny",
+        city: "Atlanta, GA",
+        media: "View Media",
+        location: "Win Park"
+    },
+    {
+        name: "Steve",
+        city: "Atlanta, GA",
+        media: "View Media",
+        location: "Freedom Park"
+    }
+];
+
+export default users;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./style/app.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
-
-
-
 
 function App() {
   return (
