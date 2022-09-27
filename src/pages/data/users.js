bc52f931459eba5b8d773a98a7715b12629a45cc
Chyanne Robbins <chyannetech@gmail.com>
2022-09-26 23:46:22 -0400

    Add Img URL to Data

--- src/pages/data/users.js
+++ src/pages/data/users.js
@@ -5,28 +5,31 @@ const users = [
         city: "Atlanta, GA",
         location: "Piedmont Park",
         reviews: "Fewer than 100 posts",
-        image: "https://m.media-amazon.com/images/M/MV5BMjExOTY3NzExM15BMl5BanBnXkFtZTgwOTg1OTAzMTE@._V1_.jpg"
+        image: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
     },
     {
         id: 2,
         name: "Chika",
         city: "Atlanta, GA",
         location: "Grant Park",
-        reviews: "500+ posts"
+        reviews: "500+ posts",
+        image: "https://images.unsplash.com/photo-1617037448248-6bd7b4a0d038?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
     },
     {
         id: 3,
         name: "Kenny",
         city: "Atlanta, GA",
         location: "Win Park",
-        reviews: "45.9k posts"
+        reviews: "45.9k posts",
+        image: "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
     },
     {
         id: 4,
         name: "Steve",
         city: "Atlanta, GA",
         location: "Freedom Park",
-        reviews: "5000+ posts"
+        reviews: "5000+ posts",
+        image: "https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1442&q=80"
     }
 ];
 
